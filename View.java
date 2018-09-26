1e0019f610173340f5ec2494b9b144c6293af98c
maxGerdes <gerdes@oculavis.de>
2018-09-26 20:57:17 +0200

    view class depicts a simple window

--- View.java
+++ View.java
@@ -1,7 +1,21 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 
 public class View extends JFrame{
 	
+	JPanel panel_1 = new JPanel();
+	JButton button_1 = new JButton();
 	
-
+	public static void main (String[] args) {
+		new View();
+	}
+	
+	public View() {
+		super("Thermonitor");
+		setSize(800,600);
+		setResizable(true);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 }
