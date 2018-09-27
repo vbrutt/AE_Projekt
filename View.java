4a9c3d754e7ae0843fafabd515555a996d1b2d6c
maxGerdes <gerdes@oculavis.de>
2018-09-27 21:13:13 +0200

    image file added, doesn't work though...

--- View.java
+++ View.java
@@ -1,14 +1,20 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
+import javax.swing.*;
+import java.awt.*;
 
 public class View extends JFrame{
 	
-	JPanel panel_1 = new JPanel();
-	JButton button_1 = new JButton();
+	private ImageIcon image1;
+	private JLabel label1;
+	private JPanel panel_1 = new JPanel();
+	private JButton button_1 = new JButton();
 	
-	public static void main (String[] args) {
-		new View();
+	public static void main (String[] args){
+		View gui =	new View();
+		
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setVisible(true);
+		gui.pack();
+		
 	}
 	
 	public View() {
@@ -16,6 +22,11 @@ public class View extends JFrame{
 		setSize(800,600);
 		setResizable(true);
 		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+//		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		image1 = new ImageIcon(getClass().getResource("knob.png"));
+		label1.setIcon(image1);
+		
+		add(label1);
 	}
 }
