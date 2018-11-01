814f56b9314b5deca71012ed4b8dbd3dc9e7a435
maxGerdes <gerdes@oculavis.de>
2018-11-01 17:38:51 +0100

    ChangeListener implementiert

--- src/View.java
+++ src/View.java
@@ -2,35 +2,51 @@
 import java.io.IOException;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class View extends JFrame {
+public class View extends JFrame 
+				  implements ChangeListener{
+	
 	private static final long serialVersionUID = 1L;
-	private JPanel jp = new JPanel();
-	private JLabel jl = new JLabel();
-	private JSlider jsld = new JSlider();
+	private JPanel _jPnl = new JPanel();
+	private JLabel _jLbl = new JLabel();
+	private JSlider _jSldr = new JSlider();
+	static int anzahlheizungen;
+	static Controller _ctrlr;
 
-	/**
-	 * 
-	 */
 	public View() {
 		setTitle("Thermonitor");
 		setVisible(true);
 		setSize(800, 600);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		jl.setIcon(new ImageIcon("C:\\Users\\verab\\Documents\\Schule\\AE\\Thermostat\\knob.png"));
-		jp.add(jl);
-		jp.add(jsld);
+		_jLbl.setIcon(new ImageIcon("C:\\Users\\verab\\Documents\\Schule\\AE\\Thermostat\\knob.png"));
+		_jSldr.addChangeListener(this);
+		_jPnl.add(_jLbl);
+		_jPnl.add(_jSldr);
 
-		add(jp);
+		add(_jPnl);
 
 		validate();
 	}
-
+	    public void stateChanged(ChangeEvent e) {
+        JSlider source = (JSlider)e.getSource();
+        if (!source.getValueIsAdjusting()) {
+            double temp = (int)source.getValue();
+            //hab noch keine HeizungsID, daher der platzhalter "heizungID"
+            _ctrlr.setTemp("heizungID",temp);
+        }    
+    }
+	    
 	public static void main(String[] args) throws NumberFormatException, IOException {	
 		String source = "";
-		Controller c = new Controller(source);
-		int anzahlheizungen = c.getHeizungen().size();
+		_ctrlr = new Controller(source);
+		anzahlheizungen = _ctrlr.getHeizungen().size();
 		new View();
 	}
 }
+
+
+
+
